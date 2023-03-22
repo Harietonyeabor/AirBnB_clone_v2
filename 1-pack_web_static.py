721cab0807545cc6d06ce79bb8e7065523a73e44
Harietonyeabor <henriettaogochukwuo@gmail.com>
2023-03-22 15:31:35 -0700

    All task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
-""" Fabfile that create a .tgz archive from
-the contents of web_static folder"""
-
-# if __name__ == '__main__':
-from fabric.api import local
+""" Function that compress a folder """
 from datetime import datetime
+from fabric.api import local
 
 
 def do_pack():
-    """Pack all the contents in the web_static directory
-    as a tar archive"""
-
+    """
+    must return the archive path if the archive has been correctly
+    generated. Otherwise, it should return None
+    """
     try:
         local("mkdir -p versions")
-        time = datetime.now()
-        date_string = '%Y%m%d%H%M%S'
-        date = time.strftime(date_string)
-
-        file_path = "versions/web_static_{}.tgz".format(date)
-        local("tar -czvf {} web_static".format(file_path))
-        return file_path
-
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        rout = "versions/web_static_{}.tgz".format(date)
+        _gzip = local("tar -cvzf {} web_static".format(rout))
+        return rout
     except Exception:
         return None
